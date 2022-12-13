1b74d9ac233832b09bb0e81c764d4ef5a8e5f8c1
Chris Hardick <chardick@icloud.com>
2022-12-12 21:32:25 -0500

    .

--- 11/111.py
+++ 11/111.py
@@ -5,6 +5,7 @@
 
 import sys
 import re
+import math
 
 class Monkey:
 
@@ -14,8 +15,21 @@ class Monkey:
         self.m_lst          = []
         self.m_op           = []
         self.m_tst          = []
-        self.m_tst_true_mk  = None
-        self.m_tst_false_mk = None
+        self.m_true_target  = None
+        self.m_false_target = None
+        self.m_num_inspect  = 0
+
+    def __str__ (self):
+
+        return "Monkey: id=%d,lst=%s,op=%s,tst=%s,true=%s,false=%s,insp=%d" % (
+            self.m_id
+            ,self.m_lst
+            ,self.m_op
+            ,self.m_tst
+            ,self.m_true_target
+            ,self.m_false_target
+            ,self.m_num_inspect
+            )
 
     def append_lst (self, lst):
         self.m_lst.append(lst)
@@ -26,11 +40,11 @@ class Monkey:
     def set_tst (self, tst):
         self.m_tst = tst
 
-    def set_tst_true_mk (self, tst_true_mk):
-        self.m_tst_true_mk = tst_true_mk
+    def set_true_target (self, tst_true_mk):
+        self.m_true_target = tst_true_mk
 
-    def set_tst_false_mk (self, tst_false_mk):
-        self.m_tst_false_mk = tst_false_mk
+    def set_false_target (self, tst_false_mk):
+        self.m_false_target = tst_false_mk
 
 
 #
@@ -58,6 +72,95 @@ for line in sys.stdin:
         m = Monkey(monkey_id)
         monkeys.append(m)
 
+    elif flds[0] == "Starting" and flds[1] == "items:":
+    
+        for x in range(2,len(flds)):
+            item = int(flds[x].replace(",",""))
+            monkeys[len(monkeys)-1].append_lst(item)
+
+    elif flds[0] == "Operation:" and flds[1] == "new" and flds[2] == "=":
+
+        monkeys[len(monkeys)-1].set_op(flds[3:len(flds)])
+        
+    elif flds[0] == "Test:" and flds[1] == "divisible" and flds[2] == "by":
+
+        monkeys[len(monkeys)-1].set_tst(["%", flds[3]])
+
+    elif (  flds[0] == "If" and 
+            flds[1] == "true:" and 
+            flds[2] == "throw" and 
+            flds[3] == "to" and
+            flds[4] == "monkey"):
+
+        monkeys[len(monkeys)-1].set_true_target(int(flds[5]))
+
+    elif (  flds[0] == "If" and 
+            flds[1] == "false:" and 
+            flds[2] == "throw" and 
+            flds[3] == "to" and
+            flds[4] == "monkey"):
+
+        monkeys[len(monkeys)-1].set_false_target(int(flds[5]))
+
     else:
         print ("%s - Not Yet Handled" % (flds[0]))
+
+
+for r in range(0,20):
+
+    print ("Round %d..." % (r+1))
+
+    for m in monkeys:
+        print (m)
+    
+        for ll in range (len(m.m_lst)):
+
+            # print ("ll=",ll)
+            item = m.m_lst[ll]
+   
+            m.m_num_inspect += 1
+
+            eval_str = ""
+    
+            for op in m.m_op:
+    
+                if op == "old": 
+                    eval_str += str(item)
+                else:
+                    eval_str += str(op)
+    
+            # print ("eval_str: ", eval_str)
+    
+            result = eval (eval_str)
+    
+            # print ("result: ", result)
+            
+            result2 = math.floor(float(result)/float(3.0))
+    
+            # print ("result2: ", result2)
+    
+            if result2 % int(m.m_tst[1]) == 0:
+                # print ("True - target=%d" % (m.m_true_target))
+
+                monkeys[m.m_true_target].m_lst.append(item)
+            
+            else:
+                # print ("False - target=%d" % (m.m_false_target))
+                
+                monkeys[m.m_false_target].m_lst.append(item)
+
+        m.m_lst.clear()
+            
+
+print ("")
+
+product = 1
+
+for m in monkeys:
+    print (m)
+
+    product *= m.m_num_inspect
+
+print ("product",product)
+
 
