10cf32cb5d22d4908a395c22c91e02c622bb6a64
Chris Hardick <chardick@icloud.com>
2022-12-21 12:17:23 -0500

    131.py

--- 13/131.py
+++ 13/131.py
@@ -3,132 +3,10 @@
 # 131.py
 #==========
 
-# pairs of packets
-# how many pairs of packets are are in the correct order
-#
-# lists and integers
-# - each list starts with [ and ends with ]
-# - each packet is always a list and appears on its own line
-#
-# if both values are integers, the lower integer should come 1st
-# - if both values are the same integer, continue checking the next part of the input
-#
-# if both values are lists, compare each item in the list to its respective item in the other list
-# if the left list runs out of items -> right order
-# if the right list runs out of items -> not the right order
-# if lists are the same length and no decision, continue checking the next part of the input
-#
-# if list vs. integer, convert the integer to a list and retry the comparison
-#
-# return the sum of the indexes in the correct order
-'''
-[1,1,3,1,1]
-[1,1,5,1,1]     -> correct order
-
-[[1],[2,3,4]]
-[[1],4]         -> correct order
-
-[9]
-[[8,7,6]]       -> not in correct order
-
-[[4,4],4,4]
-[[4,4],4,4,4]
-
-[7,7,7,7]
-[7,7,7]
-
-[]
-[3]
-
-[[[]]]
-[[]]
-
-[1, [2,[3,[4,[5,6,7]]]], 8, 9]
-[1, [2,[3,[4,[5,6,0]]]], 8, 9]
-'''
+import ch_list
 
 import sys
 
-class Expression:
-
-    def __init__(self, value=""):
-
-        self.value = value
-
-
-    def handle_list (self):
-
-        idx = 0
-
-        buf = ""
-
-        stack = []
-
-        # [1,[2,[3,[4,[5,6,7]]]],8,9]
-        # [
-        # stack=[
-
-        while idx < len(self.value):
-      
-            c = self.value[idx]
-            idx += 1
-
-            buf += c
-
-            if c == '[':
-                stack.append(c)
-            elif c == ']':
-                stack.pop()
-                if len(stack) == 0:
-                    break
-
-        self.value = buf[1:len(buf)-1]     # remove '[', ']'
-
-        return self.value
-
-
-    def handle_int (self):
-
-        idx = 0
-
-        buf = ""
-        
-        while idx < len(self.value):
-
-            c = self.value[idx]
-            idx += 1
-
-            if c == ',':
-                break
-
-            buf += c
-
-        self.value = self.value[idx:len(self.value)]
-
-        return int(buf)
-
-
-    # get_next_item
-    # - returns string for list 
-    # - otherwise returns next integer
-
-    def get_next_item (self):
-
-        if len(self.value) == 0:
-            return None
-
-        idx = 0
-
-        buf = ""
-
-        if self.value[idx] == "[":     # list
-
-            return self.handle_list()
-
-        else:       # integer
-
-            return self.handle_int()
-              
 
 # read lines from stdin
 # populate lhs[], rhs[]
@@ -166,29 +44,27 @@ get_input (lhs, rhs)
 
 num = 0
 
+answer = 0
+
+# for each expression set
+
 for i in range (len(lhs)):
 
     num += 1
     
     print ("pair#%d..." % (num))
+    print ("lhs=%s" % (lhs[i]))
+    print ("rhs=%s" % (rhs[i]))
 
-    l = Expression(lhs[i])
-    r = Expression(rhs[i])
+    l = ch_list.List(lhs[i])
+    r = ch_list.List(rhs[i])
 
-    print (lhs[i])
-    while True:
-        ll = l.get_next_item()
-        if not ll:
-            break
-        print ("ll=", ll)
+    result = ch_list.compare_lists(l,r)
 
+    print ("result=", result)
     print ("")
 
-    print (rhs[i])
-    while True:
-        rr = r.get_next_item()
-        if not rr:
-            break
-        print ("rr=", rr)
+    if result == 1:
+        answer += num
 
-    print ("")
+print ("answer:",answer)
---
+++ 13/ch_list.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+#==========
+# ch_list.py
+#==========
+
+import collections
+
+
+#
+# List
+#
+
+class List:
+
+    def __init__ (self,val):
+
+        print ("List:", val)
+
+        self.val = val
+
+        if self.val[0] == '[':
+            self.val = self.val[1:len(self.val)-1]
+
+        self.entries = collections.deque()      # queue
+
+        self._parse()
+
+
+    def next (self):
+
+        if len(self.entries) == 0:
+            print ("list=", self.val, "next=blank")
+            return ""
+
+        val = self.entries.popleft()
+        print ("list=", self.val, "next=", val)
+        return val
+
+
+    # parse self.val
+    # populate self.entries
+
+    def _parse (self):
+
+        idx = 0
+
+        buf = ""
+
+        stack = []
+
+        while idx < len(self.val):
+
+            c = self.val[idx]
+            idx += 1
+
+            if c == ',':
+                if len(stack) == 0:
+
+                    if buf.isdigit():
+                        self.entries.append(int(buf))
+                    else:
+                        self.entries.append(buf)
+                    buf = ""
+                    continue
+
+            buf += c
+
+            if c == '[':
+                stack.append(c)
+
+            elif c == ']':
+                stack.pop()
+
+        if len(buf) != 0:
+            if buf.isdigit():
+                self.entries.append(int(buf))
+            else:
+                self.entries.append(buf)
+
+
+def compare_lists (l, r):
+
+    while True:
+
+        ll = l.next()
+        rr = r.next()
+
+        if isinstance(ll,str) and len(ll) == 0 and isinstance(rr,str) and len(rr) == 0:
+            return 0
+
+        if isinstance(ll,str) and len(ll) == 0 or isinstance(rr,str) and len(rr) == 0:
+
+            # reached end of at least one list
+
+            if isinstance(ll,str) and len(ll) == 0:
+                return 1        # ok (in order)
+
+            return -1           # not ok (out of order)
+
+
+        # if here, have yet to reach end of either list
+
+
+        if isinstance(ll,str) or isinstance(rr,str):   # at least one list
+
+            lll = List((str(ll)))
+            rrr = List((str(rr)))
+
+            ret = compare_lists(lll,rrr)
+
+            if ret == 1 or ret == -1:
+                return ret
+
+        else:       # both integers
+
+            if ll < rr:         # ok (in order)
+                return 1
+
+            elif ll > rr:
+                return -1       # not ok (out of order)
+
+    return 1
